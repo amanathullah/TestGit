78f9c60568da5a991eba58d39b0844b8e94db957
amanathullah <amanath1000@gmail.com>
2018-10-20 01:31:53 +0530

    added now

--- Test/src/Abc.java
+++ Test/src/Abc.java
@@ -6,6 +6,8 @@ public class Abc {
 		System.out.println("Hello World");
 		System.out.println("new chane");
 		System.out.println("Added again");
+		System.out.println("Added again" now);
+		
 	}
 
 }
